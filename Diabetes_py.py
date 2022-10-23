e6a7f062a0976e36a7e5443e3fbb9e0faf9fb162
Gavriel Leibovitz <64496386+g-leibovitz@users.noreply.github.com>
2022-10-23 14:01:27 +0200

    First Commit - DF
    First Commit = DF

---
+++ Diabetes_py.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+df = pd.read_csv('diabetes.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
